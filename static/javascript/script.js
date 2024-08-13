d0314a0722f8077ff1be851e89e0f693673fcdd1
Filming <79931565+filming@users.noreply.github.com>
2024-08-13 18:45:29 -0400

    feat: Add playRound() to play a round of rps

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -24,6 +24,50 @@ function getHumanChoice(){
         case "scissors":
             return "scissors";
         default:
-            return null;
+            return "";
+    }
+}
+
+function playRound(humanChoice, computerChoice){
+    let gameWinner = "";
+
+    if (humanChoice === computerChoice){
+        gameWinner = "draw";
+    }
+    else if (humanChoice === "rock"){
+        if (computerChoice === "scissors"){
+            gameWinner = "human";
+        }
+        else {
+            gameWinner = "computer";
+        }
+    }
+    else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            gameWinner = "human";
+        } 
+        else {
+            gameWinner = "computer";
+        }
+    }
+    else {
+        if (computerChoice === "paper"){
+            gameWinner = "human";
+        }
+        else {
+            gameWinner = "computer";
+        }
+    }
+
+    if (gameWinner === "draw"){
+        console.log("It's a draw!");
+    } 
+    else if (gameWinner === "human"){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
     }
 }
