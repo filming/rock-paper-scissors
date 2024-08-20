200d942b18c4940f9d1f8babcc82956cf53d9bf3
Filming <79931565+filming@users.noreply.github.com>
2024-08-20 00:10:26 -0400

    refactor: Improve the game logic and code structure

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,3 +1,38 @@
+document.addEventListener("DOMContentLoaded", event => {
+    // create a zooming in effect when you hover over a player choice 
+    const playerOptions = document.querySelector(".options").querySelectorAll("img");
+
+    for (let i = 0; i < playerOptions.length; i++){
+        
+        playerOptions[i].addEventListener("mouseenter", mouseenterEvent => {
+            // make sure the button hasn't been interacted with while its been disabled (due to the game being over)
+            if (!playerOptions[i].disabled){
+
+                for (let j = 0; j < playerOptions.length; j++){
+                    if (i !== j){
+                        playerOptions[j].style.opacity = "33%";
+                    } else {
+                        playerOptions[j].style.width = "275px";
+                    }
+                }
+            }
+        });
+
+        playerOptions[i].addEventListener("mouseleave", mouseenterEvent => {
+            if (!playerOptions[i].disabled){
+
+                for (let j = 0; j < playerOptions.length; j++){
+                    if (i !== j){
+                        playerOptions[j].style.opacity = "100%";
+                    } else {
+                        playerOptions[j].style.width = "250px";
+                    }
+                }
+            }
+        });
+    }
+});
+
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -11,87 +46,129 @@ function getComputerChoice(){
     }
 }
 
-function playGame(){
-    let humanScore = 0, computerScore = 0;
-
-    const humanScoreDisplay = document.querySelector(".scores__human");
-    const computerScoreDisplay = document.querySelector(".scores__computer");
-    const resultsRound = document.querySelector(".results__round");
-    const resultsGame = document.querySelector(".results__game");
-
-    function playRound(humanChoice, computerChoice){
-        let gameWinner = "";
+function getRoundWinner(humanChoice, computerChoice){
+    let roundWinner = "";
         
-        // determine round winner
-        if (humanChoice === computerChoice){
-            gameWinner = "draw";
-        }
-        else if (humanChoice === "rock"){
-            if (computerChoice === "scissors"){
-                gameWinner = "human";
-            }
-            else {
-                gameWinner = "computer";
-            }
-        }
-        else if (humanChoice === "paper"){
-            if (computerChoice === "rock"){
-                gameWinner = "human";
-            } 
-            else {
-                gameWinner = "computer";
-            }
+    if (humanChoice === computerChoice){
+        roundWinner = "draw";
+    }
+    else if (humanChoice === "rock"){
+        if (computerChoice === "scissors"){
+            roundWinner = "human";
         }
         else {
-            if (computerChoice === "paper"){
-                gameWinner = "human";
-            }
-            else {
-                gameWinner = "computer";
-            }
+            roundWinner = "computer";
         }
-        
-        // check the round status & update the display
-        if (gameWinner === "draw"){
-            resultsRound.textContent = "This round ends in a draw!";
+    }
+    else if (humanChoice === "paper"){
+        if (computerChoice === "rock"){
+            roundWinner = "human";
         } 
-        else if (gameWinner === "human"){
-            resultsRound.textContent = `You win this round! ${humanChoice} beats ${computerChoice}.`;
-            humanScore++;
+        else {
+            roundWinner = "computer";
+        }
+    }
+    else {
+        if (computerChoice === "paper"){
+            roundWinner = "human";
         }
         else {
-            resultsRound.textContent = `You lose this round! ${computerChoice} beats ${humanChoice}.`;
-            computerScore++;
-        }
-
-        humanScoreDisplay.textContent = `Human Score: ${humanScore}`;
-        computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
-
-        if (humanScore === 5){
-            resultsGame.textContent = "You have reached 5 points, you win the game!";
-        }
-        else if (computerScore === 5){
-            resultsGame.textContent = "The computer has reached 5 points, you lose the game!";
+            roundWinner = "computer";
         }
     }
 
-    const rockBtn = document.querySelector(".player-options__rock-btn");
-    rockBtn.addEventListener("click", () => {
-        const computerSelection = getComputerChoice();
-        playRound("rock", computerSelection);
-    });
+    return roundWinner;
+}
 
-    const paperBtn = document.querySelector(".player-options__paper-btn");
-    paperBtn.addEventListener("click", () => {
-        const computerSelection = getComputerChoice();
-        playRound("paper", computerSelection);
-    });
+function getGameStatus(humanScore, computerScore){
+    let gameStatus = "none"
+
+    if (humanScore === 5){
+        gameStatus = "human";
+    }
+    else if (computerScore === 5){
+        gameStatus = "computer";
+    }
+
+    return gameStatus;
+}
+
+function gameOver(){
+    function disablePlayerOptions(){
+        const playerOptions = document.querySelector(".options").querySelectorAll("img");
     
-    const scissorsBtn = document.querySelector(".player-options__scissors-btn");
-    scissorsBtn.addEventListener("click", () => {
-        const computerSelection = getComputerChoice();
-        playRound("scissors", computerSelection);
-    });
+        for (let i = 0; i < playerOptions.length; i++){
+            playerOptions[i].disabled = true;
+            playerOptions[i].style.opacity = "33%";
+            playerOptions[i].style.width = "250px";
+
+        }
+    }
+
+    disablePlayerOptions();
+}
+
+function playGame(){
+    let humanScore = 0, computerScore = 0;
+
+    const humanScoreLabel = document.querySelector(".scores__human .scores__score");
+    const computerScoreLabel = document.querySelector(".scores__computer .scores__score");
+
+    const roundResultLabel = document.querySelector(".results__round");
+    const gameResultLabel = document.querySelector(".results__game");
+    
+    function playRound(humanChoice, computerChoice){
+
+        // determine, display and update the points of the round winner
+        const roundWinner = getRoundWinner(humanChoice, computerChoice);
+        
+        switch (roundWinner){
+            case "draw":
+                roundResultLabel.textContent = `This round ends in a draw! You both chose ${computerChoice}.`;
+                break;
+
+            case "human":
+                roundResultLabel.textContent = `You win this round! The computer chose ${computerChoice}.`;
+                humanScore++;
+                break;
+                
+            case "computer":
+                roundResultLabel.textContent = `You lose this round! The computer chose ${computerChoice}.`;
+                computerScore++;
+                break;
+        }
+
+        humanScoreLabel.textContent = humanScore;
+        computerScoreLabel.textContent = computerScore;
+
+        // determine and display the current game status
+        const gameStatus = getGameStatus(humanScore, computerScore);
+
+        switch (gameStatus){
+            case "human":
+                gameResultLabel.textContent = "You have reached 5 points, you win the game!";
+                gameOver();
+                break;
+
+            case "computer":
+                gameResultLabel.textContent = "The computer has reached 5 points, you lose the game!";
+                gameOver();
+                break;
+        }
+    }
+
+    // play a round when the user clicks on a player choice
+    const playerOptions = document.querySelector(".options").querySelectorAll("img");
+
+    for (let i = 0; i < playerOptions.length; i++){
+        playerOptions[i].addEventListener("click", clickEvent => {
+            if (!playerOptions[i].disabled){
+                
+                const computerSelection = getComputerChoice();
+                playRound(clickEvent.target.id, computerSelection);
+            }
+        });
+    }
 }
 
 playGame();
